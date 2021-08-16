b08b33806db20bfadc528e45956fd47502a300c1
Quan Nguyen H. [2] VN.Danang <mbp0037@MBP0037.local>
2021-08-16 15:02:51 +0700

    install lib bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // Cores
 import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
 // App
 import 'package:flutter_bloc_boilerplate/app.dart';
 // Services
@@ -7,11 +8,39 @@ import 'package:flutter_bloc_boilerplate/services/flavor_settings_service.dart';
 import 'package:flutter_bloc_boilerplate/services/shared_preferences_service.dart';
 // Utils
 import 'package:flutter_bloc_boilerplate/utils/constants/index.dart';
+import 'package:flutter_bloc_boilerplate/utils/helpers/index.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    LoggerHelper.log('BLOC CREATE', bloc);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    LoggerHelper.log('BLOC CHANGE', change);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    LoggerHelper.log('BLOC ERROR', stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    LoggerHelper.log('BLOC CLOSE', bloc);
+  }
+}
 
 Future<void> main() async {
   // NOTE: This is required for accessing the method channel before runApp().
   WidgetsFlutterBinding.ensureInitialized();
   await FlavorSettingsService.setFlavorSettings();
   await SharedPreferencesServices().initPrefInstance();
+  Bloc.observer = AppBlocObserver();
   runApp(App());
 }
